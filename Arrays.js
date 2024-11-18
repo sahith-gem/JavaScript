73145a08723c7442c23f07f3f5e5ee4674090a3e
Your Name <you@example.com>
2024-11-18 22:55:16 +0530

    third-commit

---
+++ Arrays.js
@@ -0,0 +1,18 @@
+let arryExample=['string1','string2',12,78.90,true];
+console.log(arryExample);
+console.log(arryExample.length);
+console.log(arryExample[4]);
+//if we try to access index out of bound of array it prints ex: undefinedarryExample[9]
+
+//Functions
+
+function sayHi(name) {
+    return( 'hi'+name);
+}
+let value = sayHi('Human');
+console.log(value);
+//if we dont explicitly return a value for a funtion in javascript by default it returns undefined
+function returnUndedined(val){
+    let name =val;
+}
+console.log(returnUndedined(20));
