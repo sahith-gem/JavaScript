6fa433f5c8ed750e67225a7c3ea9ed3bdc7e7fa8
Sahith <charysahith528@gmail.com>
2025-03-15 17:43:59 +0530

    reduce.js

--- New folder/polyfills/reduce.js
+++ New folder/polyfills/reduce.js
@@ -3,22 +3,24 @@ if(!Array.prototype.myReduce){
      
 
     
-    let startingIndex =0 ;
-    if(initialValue){
-        startingIndex =0
+    if(typeof callBack !== 'function'){
+        throw new TypeError('CallBack must be a function');
     }
-   const result =0 ;
-    for(let i=0;i<this.length ;i++){
-     result+=  callBack(initialValue , this[i], i , this)
+    const array = this ;
+    const length =array.length ;
+
+    let accumulator = initialValue !== undefined ? initialValue :array[0];
+
+    for(let i = initialValue!==undefined ?0 : 1 ;i<length ;i++){
+     accumulator =  callBack(accumulator , this[i], i , this)
 
     }
-    return result;
+    return accumulator;
 
  }
 }
 
 const array = [1,2,3,4,5]
-const result = array.myReduce((val)=>val+2 , 0)
+const result = array.myReduce((val, current) => val + current + 2, 0)
 
 console.log(result)
-//Not completed 
