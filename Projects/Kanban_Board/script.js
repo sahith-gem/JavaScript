cb415a952cd45bad2277edc0060d74dd04019be0
Sahith <charysahith528@gmail.com>
2025-03-21 18:57:21 +0530

    Completed basic implementation

--- Projects/Kanban_Board/script.js
+++ Projects/Kanban_Board/script.js
@@ -1,16 +1,55 @@
 const addTask = document.getElementById('add-task-btn')
 
+const editTask = document.getElementById('item');
+
 const todoBoard = document.getElementById('todo-board')
 
-addTask.addEventListener('click' , ()=>{
-   const input = prompt('What is the Task')
-   if(!input) return;
-    const taskCard = document.createElement('p');
-    taskCard.classList.add('item')
-    taskCard.innerText=input;
+function attachDragEvents(target){
+    target.addEventListener('dragstart', ()=>{
+        target.classList.add('flying');
+    })
+    target.addEventListener('dragend', ()=>{
+        target.classList.remove('flying')
+    })
+}
+
+editTask.addEventListener('click',()=>{
+    editTask.setAttribute('contentEditable' , 'true')
+})
+
+addTask.addEventListener('click' ,()=>{
+    const input = document.getElementById('input-task').value.trim();
+    if(!input){alert(' Task cannot be empty '); return ;}
+   const taskCard = document.createElement('p');
+    taskCard.classList.add('item');
+    taskCard.setAttribute('draggable' , true)
+    attachDragEvents(taskCard)
+    taskCard.innerText = input;
 
     todoBoard.appendChild(taskCard)
-    
-
-
 })
+
+/*
+whenever a card starts flying we can set an attribute to it as flying
+after the drag stops we will remove the attribute
+meanwhile , if the dragover monitors the flying card , and we will search for the flying attriute in the card
+and set the card to respective board
+*/
+const allBoards = document.querySelectorAll('.board')
+
+const allItems = document.querySelectorAll('.item');
+
+allItems.forEach((task)=>attachDragEvents(task))
+
+allBoards.forEach((board)=>{
+    board.addEventListener('dragover', ()=>{
+        const flyingElement = document.querySelector('.flying');
+        board.appendChild(flyingElement)
+        console.log('flying')
+    })
+})
+
+/*
+The element from the current board gets removed and gets add up in another board
+just because we are changing the reference of the node to different parent in dom with appendchild
+*/ 
